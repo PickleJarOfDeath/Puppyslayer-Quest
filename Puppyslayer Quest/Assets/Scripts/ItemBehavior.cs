7397540f0560146cf196ddddbd08fbb2cab47ff6
PickleJarOfDeath <dylanf3d@gmail.com>
2022-02-04 13:49:08 -0700

    however many times is the charm

---
+++ Puppyslayer Quest/Assets/Scripts/ItemBehavior.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            Destroy(this.transform.parent.gameObject);
+
+            Debug.Log("Item collected!");
+        }
+    }
+}
